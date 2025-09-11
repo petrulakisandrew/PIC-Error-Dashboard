66ee36ce14f8b794a32085105b3c08e4336436b3
Andrew Petrulakis <petrulakisandrew@gmail.com>
2025-09-11 13:12:53 -0500

    Initial Commit Post Data Cleansing

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+#Initiate GIT
+
+#Importing Excel DF
+df = pd.read_excel(
+    'C:/Users/Andrew Petrulakis/Desktop/Dashboards/PIC Error Dashboard/Raw Data/Fatal Errors Test 2025-08-08 Updated.xlsx',
+    header=26   #
+)
+
+df = df.drop(["Rec Nbr in Error","Section","Development Number", "Building Number", "Building Number Entrance","Unit Number","PHA Use Only1","PHA Use Only2","PHA Use Only3","PHA Use Only4","PHA Use Only5"], axis=1)
+
+
+# print(df.head())
+
+#Caseworker Column
+conditions = [
+    (df["Last Name"].str.strip().str.lower() >= "a" ) & (df["Last Name"].str.strip().str.lower() <= "bel"),
+    (df["Last Name"].str.strip().str.lower() >= "ben" ) & (df["Last Name"].str.strip().str.lower() <= "col"),
+    (df["Last Name"].str.strip().str.lower() >= "con" ) & (df["Last Name"].str.strip().str.lower() <= "ful"),
+    (df["Last Name"].str.strip().str.lower() >= "gad" ) & (df["Last Name"].str.strip().str.lower() <= "haw"),
+    (df["Last Name"].str.strip().str.lower() >= "hay" ) & (df["Last Name"].str.strip().str.lower() <= "lev"),
+    (df["Last Name"].str.strip().str.lower() >= "lew" ) & (df["Last Name"].str.strip().str.lower() <= "pau"),
+    (df["Last Name"].str.strip().str.lower() >= "paw" ) & (df["Last Name"].str.strip().str.lower() <= "ste"),
+    (df["Last Name"].str.strip().str.lower() >= "sti" ) & (df["Last Name"].str.strip().str.lower() <= "z"),
+]
+
+caseworkers = ["Candice","Gail","Scott","Cristine","Sandra","Ozury","Occupancy","Mary"]
+
+df["Caseworker"] = np.select(conditions, caseworkers, default="Other")
+
+
+df = df.drop(df[df["Error Type"] == " WARNING"].index)
+# print(df)
+
+col1, = st.columns(1)
+col1.metric("Total Fatal Errors", df["Error Number"].count())
